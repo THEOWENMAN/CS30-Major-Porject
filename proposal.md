48dd809bdc70c3d94b01d3607bce00b956287d0e
THEOWENMAN <owentang799@gmail.com>
2025-04-28 14:16:07 -0600

    Talked to Mr. Schellenberg about proposal and modified it a bit. Coded a basic start version of drawing the characters and bullets, then created a test code

--- proposal.md
+++ proposal.md
@@ -1,12 +1,12 @@
 # CS30 Major Project
 
 ## Description
-Tussle Galaxy: A 3 v 3 multiplayer 2D shooter game inspired by Brawl Stars. The Game will be coded with p5.js and p5.party; players will join a shared lobby and battle using their unique characters.
+Tussle Galaxy: A multiplayer 2D shooter game inspired by Brawl Stars. The Game will be coded with p5.js and p5.party; players will join a shared lobby and battle using their unique characters.
 
 ## Needs to have List
-- Multiplayer (p5.Party): Real-time 3v3 battles with players join via other devices.
+- Multiplayer (p5.Party): Real-time multiplayer battles joining via other devices.
 - Map/Mode: Incorporate a simple map that allows player movenment and shooting, while having multiple obstacles. 
-- 3 Different Characters: Each charater will have unique shooting mechanics (eg, spread shot, long-range, burst, single, etc) and different movement patterns or speeds.
+- Sound Effects: Sound effects for the character shooting and epic game music.
 - Shooting Function: Using mouse to aim and shoot bullets.
 - Health Function: Include a basic health system, players can take damage and get knocked out.
 - Respawn and End Condition: Either respawn the players for a set amount of time or end if one team is all wiped out.
@@ -15,11 +15,11 @@ Tussle Galaxy: A 3 v 3 multiplayer 2D shooter game inspired by Brawl Stars. The 
 
 ## Nice to have List
 - Advance Designs: Include shooting animations, wall hit effects, polished visual style, and more.
-- Sound Effects: Different sound Effects for each type of character shooting and self recorded voice lines and music.
 - Advanced Maps/Modes: Include more complex maps and different game modes with zones, power-ups: speed boosts, health kits, and ammo refills.
 - Character Special abilities: Each character has a special move and potentially have a hyper super that modifies the super attack.
 - Emotes: Emotes or sprays 
-- 
+- 3 Different Characters: Each charater will have unique shooting mechanics (eg, spread shot, long-range, burst, single, etc) and different movement patterns or speeds.
+- Sound Effects additional: Different sound effects for each type of character shooting and self recorded voice lines.
 
 
 
--- sketch.js
+++ sketch.js
@@ -1,9 +1,95 @@
-// CS30 Major Project: Tussle Galaxy
-// Owen Tang
-// April 17, 2025
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// // CS30 Major Project: Tussle Galaxy
+// // Owen Tang
+// // April 17, 2025
+// //
+// // Extra for Experts:
+// // - describe what you did to take this project "above and beyond"
+
+
+// let guests, shared, my;
+// let bulletsArray = [];
+
+// function preload(){
+//   partyConnect("wss://demoserver.p5Party.org");
+//   shared = partyLoadShared("shared", {bullets: []});
+//   my = partyLoadMyShared();
+//   guests = partyLoadGuestShareds();
+// }
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   my.longRangeCharacter = {x: random(width), y: random(height), HP: 50,};
+//   my.mediumRangeCharacter = {x: random(width), y: random(height), HP: 70,};
+//   my.shortRangeCharacter = {x: random(width), y: random(height), HP: 100,};
+//   partySubscribe("createBullet", onCreateBullet);
+//   startGame();
+// }
+
+// function draw() {
+//   background(220);
+//   for (let p of guests){
+//     drawCharacter(p.longRangeCharacter, "red");
+//     drawCharacter(p.mediumRangeCharacter, "orange");
+//     drawCharacter(p.shortRangeCharacter, "yellow");
+//   }
+
+//   drawCharacter(my.longRangeCharacter, "blue");
+//   drawCharacter(my.mediumRangeCharacter, "blue");
+//   drawCharacter(my.shortRangeCharacter, "blue");
+
+//   for(let b of shared.bullets){
+//     ellipse(b.x,b.y, 10);
+//   }
+// }
+
+// function drawCharacter(character, color){
+//   if(!character){
+//     return;
+//   }
+//   fill(color);
+//   ellipse(character.x, character.y, 40);
+//   fill(0);
+//   text("HP:" + character.HP, character.x, character.y + 30);
+// }
+
+// function startGame(){
+//   shared.bullets.forEach(stepBullet);
+// }
+
+// function stepBullet(b){
+//   b.x += b.dx;
+//   b.y += b.dy;
+// }
+
+// function mousePressed(){
+//   createBullet(my.longRangeCharacter.x, my.longRangeCharacter, mouseX, mouseY);
+// }
+
+// function createBullet(){
+//   let direction = createVector(mouseX - my.longRangeCharacter.x, mouseY - my.longRangeCharacter.y);
+//   direction.normalize();
+//   direction.mult(4);
+//   let position = createVector(my.longRangeCharacter.x, my.longRangeCharacter.y);
+//   position.x += direction.x * (40/4);
+//   position.y += direction.y * (40/4);
+//   let bullet = {
+//     pos: position,
+//     vel: direction,
+//   };
+//   bulletsArray.push(bullet);
+
+
+
+
+
+// }
+
+// function onCreateBullet(){
+//   shared.bullets.push(bullet);
+// }
+
+
+
 
 
 let guests, shared, my;
@@ -11,37 +97,42 @@ let guests, shared, my;
 function preload(){
   partyConnect("wss://demoserver.p5Party.org");
   shared = partyLoadShared("shared", {bullets: []});
-  my = sharedLoadMyShared();
-  guests = partyLoadGuestShared();
+  my = partyLoadMyShared();
+  guests = partyLoadGuestShareds();
 }
-
-
-
-
-
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   my.longRangeCharacter = {x: random(width), y: random(height), HP: 50,};
-  my.mediumRangeCharacter = {x: random(width), y: random(height), HP: 70,};
-  my.shortRangeCharacter = {x: random(width), y: random(height), HP: 100,};
   partySubscribe("createBullet", onCreateBullet);
 }
 
 function draw() {
-  background(220);
+  if(partyIsHost()){
+    stepGame();
+    guests: guests;
+  }
+  for (let p of guests){
+    drawCharacter(p.longRangeCharacter, "red");
+  }
+  drawCharacter(my.longRangeCharacter, "blue");
+  for(let b of shared.bullets){
+    ellipse(b.x,b.y, 10);
+  }
 }
 
-
-
-
-
-function startGame(){
+function stepGame(){
   shared.bullets.forEach(stepBullet);
 }
 
 function stepBullet(b){
   b.x += b.dx;
   b.y += b.dy;
+}
+
+function onCreateBullet(b){
+  if(partyIsHost()){
+    shared.bullets.push(b);
+  }
+  
 }
